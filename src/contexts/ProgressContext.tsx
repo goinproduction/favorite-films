d7338c3a8bf591d305cf58acc60c8a3eb07663ba
viincyy <viincyy126@gmail.com>
2021-09-02 17:10:36 +0700

    DefaultContextProgress

--- src/contexts/ProgressContext.tsx
+++ src/contexts/ProgressContext.tsx
@@ -3,12 +3,19 @@ import { createContext, ReactNode } from 'react';
 interface ProgressContextProviderProps {
     children: ReactNode;
 }
+
+interface DefaultContextProgress {
+    lastTime: string;
+    status: string;
+}
+
 const defaultProgress = {
     lastTime: '02/09/2021',
     status: 'In Progress',
 };
 
-export const ProgressContext = createContext(defaultProgress);
+export const ProgressContext =
+    createContext<DefaultContextProgress>(defaultProgress);
 
 const ProgressContextProvider = ({
     children,
