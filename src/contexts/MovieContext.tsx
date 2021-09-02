952b47b37435f3d57aad56816569bc5de7ebba26
viincyy <viincyy126@gmail.com>
2021-09-02 19:48:17 +0700

    Create movie context

---
+++ src/contexts/MovieContext.tsx
@@ -0,0 +1,55 @@
+import { PropTypes } from '@material-ui/core';
+import { createContext, ReactNode, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+interface MovieContextProps {
+    children: ReactNode;
+}
+
+interface Movie {
+    id: string;
+    title: string;
+}
+
+interface MovieDefaultContext {
+    movies: Movie[];
+    addMovie: (title: string) => void;
+    deleteMovie: (id: string) => void;
+}
+
+const movieDefaultContextData = {
+    movies: [],
+    addMovie: () => {},
+    deleteMovie: () => {},
+};
+
+export const MovieContext = createContext<MovieDefaultContext>(
+    movieDefaultContextData
+);
+
+const MovieContextProvider = ({ children }: MovieContextProps) => {
+    const [movies, setMovies] = useState<Movie[]>(
+        movieDefaultContextData.movies
+    );
+
+    const addMovie = (title: string) =>
+        setMovies([
+            ...movies,
+            {
+                id: uuidv4(),
+                title,
+            },
+        ]);
+
+    const deleteMovie = (id: string) =>
+        setMovies(movies.filter((movie) => movie.id !== id));
+
+    const movieContextData = { movies, addMovie, deleteMovie };
+    return (
+        <MovieContext.Provider value={movieContextData}>
+            {children}
+        </MovieContext.Provider>
+    );
+};
+
+export default MovieContextProvider;
